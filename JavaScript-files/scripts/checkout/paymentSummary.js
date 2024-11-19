aa3f63f5387a4482faa399a4adadfe6bda97cfd8
Priapisman677 <expeliamm02@hotmail.com>
2024-11-18 20:15:38 -0700

    Create order using the backend

--- JavaScript-files/scripts/checkout/paymentSummary.js
+++ JavaScript-files/scripts/checkout/paymentSummary.js
@@ -1,56 +1,90 @@
-import { cart } from '../../data/cart.js';
-import { getProduct } from '../../data/products.js';
-import { getDeliveryOption } from '../../data/deliveryOptions.js';
-import formatCurrency from '../utils/money.js'
+import { cart } from "../../data/cart.js";
+import { getProduct } from "../../data/products.js";
+import { getDeliveryOption } from "../../data/deliveryOptions.js";
+import formatCurrency from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
-export function renderPaymentSummary(){
+export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
 
-  cart.forEach((cartItem)=>{
-    const product = getProduct(cartItem.productId)
-    productPriceCents += product.priceCents * cartItem.quantity
+  cart.forEach((cartItem) => {
+    const product = getProduct(cartItem.productId);
+    productPriceCents += product.priceCents * cartItem.quantity;
 
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
-    shippingPriceCents += deliveryOption.priceCents
+    shippingPriceCents += deliveryOption.priceCents;
   });
-   const totalBeforeTaxCents = productPriceCents + shippingPriceCents
-   const taxCents = totalBeforeTaxCents * 0.1;
-   const totalCents = totalBeforeTaxCents + taxCents
-  
-   const paymentSummaryHTML = `
+  const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
+  const taxCents = totalBeforeTaxCents * 0.1;
+  const totalCents = totalBeforeTaxCents + taxCents;
+
+  const paymentSummaryHTML = `
     <div class="payment-summary-title">
             Order Summary
           </div>
 
           <div class="payment-summary-row">
             <div>Items (3):</div>
-            <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
+            <div class="payment-summary-money">$${formatCurrency(
+              productPriceCents
+            )}</div>
           </div>
 
           <div class="payment-summary-row">
             <div>Shipping &amp; handling:</div>
-            <div class="payment-summary-money">$${formatCurrency(shippingPriceCents)}</div>
+            <div class="payment-summary-money">$${formatCurrency(
+              shippingPriceCents
+            )}</div>
           </div>
 
           <div class="payment-summary-row subtotal-row">
             <div>Total before tax:</div>
-            <div class="payment-summary-money">${formatCurrency(totalBeforeTaxCents)}</div>
+            <div class="payment-summary-money">${formatCurrency(
+              totalBeforeTaxCents
+            )}</div>
           </div>
 
           <div class="payment-summary-row">
             <div>Estimated tax (10%):</div>
-            <div class="payment-summary-money">$${formatCurrency(taxCents)}</div>
+            <div class="payment-summary-money">$${formatCurrency(
+              taxCents
+            )}</div>
           </div>
 
           <div class="payment-summary-row total-row">
             <div>Order total:</div>
-            <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
+            <div class="payment-summary-money">$${formatCurrency(
+              totalCents
+            )}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary
+          js-place-order">
             Place your order
           </button>
    `;
-   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+  document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHTML;
+
+  document.querySelector(".js-place-order").addEventListener("click", async () => {
+    try{
+      const response = await fetch("https://supersimplebackend.dev/orders", {
+        method: "POST",
+        headers: { 
+          "Content-Type": "application/json" 
+        },
+        body:  JSON.stringify({cart: cart})
+      });
+        //* The code below will run after the await of fetch, This is the natural flow of code using async:
+        //* However, remember that response.json() is a promise, so we need to await it:
+        const order = await response.json();
+        addOrder(order);
+    } catch (error) {
+      console.log('unexpected error:',error);
+    }
+    //* what is below is going to replace the URL after the slash on our current page.
+    window.location.href  = 'orders.html'
+
+  });
 }
+console.log(cart)
